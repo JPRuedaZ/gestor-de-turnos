3b8ee2df784bb73f85b9602713816b8f3fd28cdf
JPRuedaZ <juanpabloruedazo@gmail.com>
2024-02-28 11:13:10 -0500

    practicando ts

--- back/dist/index.js
+++ back/dist/index.js
@@ -1,3 +1,16 @@
 "use strict";
-const primeraPalabra = 'Hola';
-console.log(primeraPalabra);
+const calcularAre = (lado1, lado2) => {
+    return lado1 * lado2;
+};
+console.log(calcularAre(5, 6));
+const presentarUsuario = (name, apellido, edad = 'No especificada') => {
+    if (!apellido) {
+        console.log(`Nombre: ${name}, Edad: ${edad}`);
+    }
+    else {
+        console.log(`Nombre: ${name}, Apellido: ${apellido}, Edad: ${edad}`);
+    }
+};
+console.log(presentarUsuario('Homero'));
+console.log(presentarUsuario('Homero', 'Simpson'));
+console.log(presentarUsuario('Homero', 'Simpson', '39'));
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,2 +1,17 @@
-const primeraPalabra :string = 'Hola';
-console.log(primeraPalabra)
+const calcularAre = (lado1:number, lado2:number): number => {
+    return lado1 * lado2
+}
+console.log(calcularAre(5,6));
+
+const presentarUsuario = (name: string, apellido?: string, edad: string = 'No especificada'): void => {
+    if(!apellido) {
+        console.log(`Nombre: ${name}, Edad: ${edad}`);
+    } else {
+        console.log(`Nombre: ${name}, Apellido: ${apellido}, Edad: ${edad}`);
+    }
+}
+console.log(presentarUsuario('Homero'));
+console.log(presentarUsuario('Homero', 'Simpson'));
+console.log(presentarUsuario('Homero', 'Simpson', '39'));
+
+
