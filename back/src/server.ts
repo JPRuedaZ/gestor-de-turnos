ca5d491bd63c672888140473194be1d04b367ed6
JPRuedaZ <juanpabloruedazo@gmail.com>
2024-03-29 19:07:40 -0500

    final m3

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,5 +1,8 @@
+//Importamos Express para levantar el servidor con node y typescript.
 import express from "express";
+//Importamos el enrutador que envia la peticion que llega al servidor a las rutas.
 import router from "./routes/endpoint";
+//
 import morgan from "morgan";
 import cors from "cors";
 
