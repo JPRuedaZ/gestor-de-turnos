d2a99fb13805579cf0b57000278f58424d44072b
JPRuedaZ <juanpabloruedazo@gmail.com>
2024-04-30 15:06:05 -0500

    El trabajo esta funcionando , falta arreglarle los estilos e implementar unas views y el tema del responsive

--- back/src/utils/appointments.ts
+++ back/src/utils/appointments.ts
@@ -19,6 +19,6 @@ export const appointments: IAppointments[] = [
         id: 3,
         date: '2024-01-01',
         hour: '10:00',
-        userId: 1,
+        userId: 2,
         status: 'Active'
     }]
